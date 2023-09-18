00d332b467e24ab0d7cc4259281f169a6e7828d0
crowdbotics-dev <engineering@crowdbotics.com>
2023-09-18 13:49:29 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Animal(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=255,)
+    age = models.BigIntegerField()
